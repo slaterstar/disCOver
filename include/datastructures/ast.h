dd02a3dadc9f28d90130a266a005fe39d2f026f7
Sterling <gips@uci.edu>
2026-05-14 02:03:22 -0700

    Converted binary to ternary

--- include/datastructures/ast.h
+++ include/datastructures/ast.h
@@ -7,19 +7,19 @@
 #include "hash.h"
 // Hash-Consed AST Node
 // For our hash we use a bucket-based approach, hence why we have a `next` pointer
+// Making it a ternary is necessary for a branch operator
 typedef struct ast_node {
     IROpcode opcode;
-    uint64_t hash;
-    union {
-        uint64_t value;
-        struct{
-            struct ast_node* left_child;
-            struct ast_node* right_child;
-        } bin;
-    } data;
-    struct ast_node* next; // The next node in the bucket
+    uint32_t hash;
+
+    // The Universal Fields
+    struct ast_node* c1; // Used for: Left Child OR Condition
+    struct ast_node* c2; // Used for: Right Child OR True Target
+    struct ast_node* c3; // Used for: False Target
+    uint64_t value;      // Used for: Immediates/Registers
+
+    struct ast_node* next; // Hash table bucket chain
 } ast_node_t;
 
-uint32_t hash_node(IROpcode opcode, ast_node_t* l, ast_node_t* r, int val);
-
+ast_node_t* make_node(IROpcode op, ast_node_t* c1, ast_node_t* c2, ast_node_t* c3, int imm);
 #endif // AST_H
