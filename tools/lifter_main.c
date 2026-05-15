feb346e44465bef4988818fb83d5c99f7c2a0e63
Sterling <gips@uci.edu>
2026-05-14 19:03:07 -0700

    Printed specific memory address

--- tools/lifter_main.c
+++ tools/lifter_main.c
@@ -66,8 +66,11 @@ int main(int argc, char *argv[]){
         }
     }
 
+
+
     // Print IR instructions
     for(int i = 0; i < ctx->state_map->size; i++) {
+        printf("StateMapEntry %d: address=%u\n", i, ((StateMapEntry*)ctx->state_map->data)[i].address);
         print_node(((StateMapEntry*)ctx->state_map->data)[i].node, 0);
     }
 
